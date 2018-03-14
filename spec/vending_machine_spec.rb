4380f0b805141d5a49d57f04f620a8b548010e3d
Arthur Granado <agranado2k@gmail.com>
2018-03-14 09:15:23 +0000

    Create basic specs

--- spec/vending_machine_spec.rb
+++ spec/vending_machine_spec.rb
@@ -1,5 +1,7 @@
 require 'rspec'
 
+require_relative '../lib/cleo'
+
 describe Cleo::VendingMachine do
   context 'when init' do
     context 'load products' do
@@ -31,46 +33,48 @@ describe Cleo::VendingMachine do
     end
   end
 
-  context 'purchase process' do
-    context 'choose a product' do
-      it { expect(subject.product).to_not be_nil }
-      it { expect(subject.total).to eq(product.price) }
-      it { expect(subject.paid).to eq(0) }
-      it { expect(subject.balance).to eq(product.price*-1) }
-      it { expect(subject.status).to eq('pending') }
-      it { expect(subject.change).to eq(0) }
-      it { expect(subject.change_list).to be_empty }
-    end
-
-    context 'check out payment' do
-      context 'insert insufficient payment' do
+  describe Cleo::Purchase do
+    context 'purchase process' do
+      context 'choose a product' do
+        it { expect(subject.product).to_not be_nil }
         it { expect(subject.total).to eq(product.price) }
-        it { expect(subject.paid).to eq(value_inserted) }
-        it { expect(subject.balance).to eq(value_inserted - product.price) }
+        it { expect(subject.paid).to eq(0) }
+        it { expect(subject.balance).to eq(product.price*-1) }
         it { expect(subject.status).to eq('pending') }
         it { expect(subject.change).to eq(0) }
         it { expect(subject.change_list).to be_empty }
-        it { expect(subject.message).to eq('Insufficient money. Insert more, please.') }
       end
 
-      context 'insert exactaly the price as payment' do
-        it { expect(subject.total).to eq(product.price) }
-        it { expect(subject.paid).to eq(product.price) }
-        it { expect(subject.balance).to eq(0) }
-        it { expect(subject.status).to eq('paid') }
-        it { expect(subject.change).to eq(0) }
-        it { expect(subject.change_list).to be_empty }
-        it { expect(subject.message).to eq('Payment completed') }
-      end
+      context 'check out payment' do
+        context 'insert insufficient payment' do
+          it { expect(subject.total).to eq(product.price) }
+          it { expect(subject.paid).to eq(value_inserted) }
+          it { expect(subject.balance).to eq(value_inserted - product.price) }
+          it { expect(subject.status).to eq('pending') }
+          it { expect(subject.change).to eq(0) }
+          it { expect(subject.change_list).to be_empty }
+          it { expect(subject.message).to eq('Insufficient money. Insert more, please.') }
+        end
 
-      context 'insert more than the price as payment' do
-        it { expect(subject.total).to eq(product.price) }
-        it { expect(subject.paid).to eq(value_inserted) }
-        it { expect(subject.balance).to eq(value_inserted - product.price) }
-        it { expect(subject.status).to eq('paid') }
-        it { expect(subject.change).to eq(value_inserted - product.price) }
-        it { expect(subject.change_list).to_not be_empty }
-        it { expect(subject.message).to eq('Payment completed. Get your change!') }
+        context 'insert exactaly the price as payment' do
+          it { expect(subject.total).to eq(product.price) }
+          it { expect(subject.paid).to eq(product.price) }
+          it { expect(subject.balance).to eq(0) }
+          it { expect(subject.status).to eq('paid') }
+          it { expect(subject.change).to eq(0) }
+          it { expect(subject.change_list).to be_empty }
+          it { expect(subject.message).to eq('Payment completed') }
+        end
+
+        context 'insert more than the price as payment' do
+          it { expect(subject.total).to eq(product.price) }
+          it { expect(subject.paid).to eq(value_inserted) }
+          it { expect(subject.balance).to eq(value_inserted - product.price) }
+          it { expect(subject.status).to eq('paid') }
+          it { expect(subject.change).to eq(value_inserted - product.price) }
+          it { expect(subject.change_list).to_not be_empty }
+          it { expect(subject.message).to eq('Payment completed. Get your change!') }
+        end
       end
     end
   end
