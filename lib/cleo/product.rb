7c78eec61bd395de858abe3830f43b1c89381720
Arthur Granado <agranado2k@gmail.com>
2018-03-14 09:37:53 +0000

    Create Product class

--- lib/cleo/product.rb
+++ lib/cleo/product.rb
@@ -1,5 +1,11 @@
 module Cleo
   class Product
+    attr_accessor :name, :price, :quantity
 
+    def initialize(params)
+      @name = params[:name]
+      @price = params[:price]
+      @quantity = params[:quantity]
+    end
   end
 end
--- spec/product_spec.rb
+++ spec/product_spec.rb
@@ -1,6 +1,9 @@
 require 'rspec_helper'
 
 describe Cleo::Product do
+  let(:params) { { name: 'product_1', price: 2.50, quantity: 3 } }
+  subject { described_class.new params }
+
   context 'when create' do
     it { expect(subject.name).to eq(params[:name]) }
     it { expect(subject.price).to eq(params[:price]) }
